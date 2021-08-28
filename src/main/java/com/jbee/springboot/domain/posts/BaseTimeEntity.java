a0eb2cb9644965b670fcbcd94eb53b8f2a83e122
tlswo <shin0343@mail.hongik.ac.kr>
2021-08-28 16:35:26 +0900

    JPA Auditing 이용하여 생성시간/수정시간 남김 자동화

---
+++ src/main/java/com/jbee/springboot/domain/posts/BaseTimeEntity.java
@@ -0,0 +1,30 @@
+package com.jbee.springboot.domain.posts;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+// Entity들의 createdDate, modifiedDate를 자동으로 관리하는 역할
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+}
--- src/test/java/com/jbee/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jbee/springboot/web/PostsApiControllerTest.java
@@ -94,6 +94,6 @@ public class PostsApiControllerTest {
 
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
-        assertThat(all.get(0).getContent()).isEqualTo(expectedContent)
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
 }
