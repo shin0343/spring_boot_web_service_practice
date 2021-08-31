f81d1e9c81ce6e93435b4522e928ef7f5075c6ed
tlswo <shin0343@mail.hongik.ac.kr>
2021-08-31 22:57:36 +0900

    [테스트 코드에 시큐리티 적용]

--- src/main/java/com/jbee/springboot/Application.java
+++ src/main/java/com/jbee/springboot/Application.java
@@ -1,9 +1,8 @@
 package com.jbee.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+//@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/jbee/springboot/HelloControllerTest.java
+++ src/test/java/com/jbee/springboot/HelloControllerTest.java
@@ -5,23 +5,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
-
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -31,17 +32,18 @@ public class HelloControllerTest {
                 .andExpect((ResultMatcher) content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
         mvc.perform(
-                get("/hello/dto")
-                        .param("name",name)
-                        .param("amount",String.valueOf(amount))
-        ).andExpect(status().isOk())
-                .andExpect(jsonPath("$.name",is(name)))
-                .andExpect((ResultMatcher) jsonPath("$.amount",is(amount)));
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
